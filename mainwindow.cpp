721776b6805418415e1c358ba02710417a6c273d
Felipe Soares Barbosa Silveira <felipeiturama@gmail.com>
2019-10-02 18:00:09 -0300

    Primeiro commit

---
+++ mainwindow.cpp
@@ -0,0 +1,90 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow),
+      m_nam(new QNetworkAccessManager(this))
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionSair_triggered()
+{
+    close();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    close();
+}
+
+void MainWindow::on_btn_atualizar_clicked()
+{
+//    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    QNetworkRequest request;
+    request.setUrl(QUrl(ui->ln_servidor->text()));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String(
+                          "application/json"));
+//    request.setRawHeader("User-Agent", "MyOwnBrowser 1.0");
+
+    QJsonObject dados_local{
+        {"teste", "teste"}
+                           };
+//    QNetworkReply *reply = m_nam->post(request, QJsonDocument(dados_local).toJson());
+    QNetworkReply *reply = m_nam->get(request);
+    connect(reply, &QNetworkReply::finished, this,
+            &MainWindow::slotFinished);
+//    connect(m_nam, &QNetworkAccessManager::finished, this,
+//            &MainWindow::slotFinished);
+    connect(reply, &QIODevice::readyRead, this,
+            &MainWindow::slotReadyRead);
+    connect(reply,
+            QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error),
+            this, &MainWindow::slotError);
+    connect(reply, &QNetworkReply::sslErrors,
+            this, &MainWindow::slotSslErrors);
+}
+
+void MainWindow::slotFinished()
+{
+    QNetworkReply * reply = qobject_cast<QNetworkReply *>(sender());
+    reply->deleteLater();
+
+    ui->lbl_horas->setText(reply->readAll());
+    qDebug() << reply->readAll();
+//    const QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
+//    const QJsonObject obj = doc.object();
+
+//    if (obj.isEmpty()) {
+//        qDebug() << obj;
+//    }
+    qDebug() << "slot Finished";
+}
+
+void MainWindow::slotReadyRead()
+{
+    qDebug() << "slot Ready Read";
+}
+
+void MainWindow::slotError()
+{
+    qDebug() << "slot Error";
+}
+
+void MainWindow::slotSslErrors()
+{
+    qDebug() << "slot Error SSL";
+}
---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+//#include <QObject>
+//#include <QNetworkRequest>
+//#include <QNetworkAccessManager>
+//#include <QNetworkReply>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+//class QNetworkReply;
+QT_END_NAMESPACE
+
+class QNetworkAccessManager;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void slotReadyRead();
+    void slotError();
+    void slotSslErrors();
+    void slotFinished();
+private slots:
+    void on_actionSair_triggered();
+
+    void on_pushButton_clicked();
+
+    void on_btn_atualizar_clicked();
+
+private:
+
+    Ui::MainWindow *ui;
+    QNetworkAccessManager *m_nam;
+
+//    QNetworkAccessManager::Operation m_method;
+//    QNetworkRequest m_request;
+};
+#endif // MAINWINDOW_H
